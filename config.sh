be223587775d5c6d7d64326ff1dfcafd8278645b
cyberbot <cyberbot@cyberbot-laptop.(none)>
2013-12-02 00:26:15 +0900

    config file added

---
+++ config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+BASEURL=localhost:8090
+USERNAME=admin
+USERPASS=admin
+
+SEEDURL[0]=http://localhost:8090/display/ds/Welcome+to+Confluence
--- login.sh
+++ login.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-URL=localhost8090
+source ./config.sh
 
-curl 'http://localhost:8090' -v -c new_cookie.txt
-
-cat new_cookie.txt > old_cookie.txt
-
-curl 'http://localhost:8090/homepage.action' -v -b old_cookie.txt -c new_cookie.txt
+curl "http://${BASEURL}" -v -c new_cookie.txt
 
 cat new_cookie.txt > old_cookie.txt
 
-curl 'http://localhost:8090/login.action?os_destination=%2Fhomepage.action' -v -b old_cookie.txt -c new_cookie.txt
+curl "http://${BASEURL}/homepage.action" -v -b old_cookie.txt -c new_cookie.txt
 
 cat new_cookie.txt > old_cookie.txt
 
-curl -d 'os_username=admin&os_password=admin&os_cookie=true&os_destination=%2Fhomepage.action' 'http://localhost:8090/dologin.action' -v -b old_cookie.txt -c new_cookie.txt
+curl "http://${BASEURL}/login.action?os_destination=%2Fhomepage.action" -v -b old_cookie.txt -c new_cookie.txt
 
 cat new_cookie.txt > old_cookie.txt
 
-curl 'http://localhost:8090/homepage.action' -v -b old_cookie.txt -c new_cookie.txt
+curl -d "os_username=${USERNAME}&os_password=${USERPASS}&os_cookie=true&os_destination=%2Fhomepage.action" "http://${BASEURL}/dologin.action" -v -b old_cookie.txt -c new_cookie.txt
 
 cat new_cookie.txt > old_cookie.txt
 
-curl 'http://localhost:8090/dashboard.action' -v -b old_cookie.txt -c new_cookie.txt
+curl "http://${BASEURL}/homepage.action" -v -b old_cookie.txt -c new_cookie.txt
+
+cat new_cookie.txt > old_cookie.txt
+
+curl "http://${BASEURL}/dashboard.action" -v -b old_cookie.txt -c new_cookie.txt
 
 cat new_cookie.txt > old_cookie.txt
